9a7d8be091577c249fb77224253d1b08c76d0dca
Oswaldo Rubio <osrufung@gmail.com>
2012-12-20 12:02:58 +0100

    bug fixes
    outlets from view should be weak properties

--- IrregularVerbs/ViewController.h
+++ IrregularVerbs/ViewController.h
@@ -17,10 +17,10 @@
 //current show state(0 = present, 1 = past, 2 = participle)
 @property int current_State;
 //outlets for labels
-@property (nonatomic, strong) IBOutlet UILabel *labelPresent;
-@property (nonatomic, strong) IBOutlet UILabel *labelPast;
-@property (nonatomic, strong) IBOutlet UILabel *labelParticiple;
-@property (nonatomic, strong) IBOutlet UILabel *labelTranslation;
+@property (nonatomic, weak) IBOutlet UILabel *labelPresent;
+@property (nonatomic, weak) IBOutlet UILabel *labelPast;
+@property (nonatomic, weak) IBOutlet UILabel *labelParticiple;
+@property (nonatomic, weak) IBOutlet UILabel *labelTranslation;
 //button tapped event
 - (IBAction)screenTapped:(id)sender;
 //logic method
