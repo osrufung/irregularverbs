d2dd9a672c9ac1182482ceedc6fcb4e51beb6688
Vicente <Vicente@Vicentes-MacBook.local>
2013-02-02 17:06:56 +0100

    CardsTableViewController cleaning

--- IrregularVerbs/ColorsDefinition.h
+++ IrregularVerbs/ColorsDefinition.h
@@ -13,5 +13,4 @@
 #define TURQUESA_TINT  [UIColor colorWithRed:0.0/255.0 green:218.0/255.0 blue:194.0/255.0 alpha:1.0]
 #define ROJO_TINT [UIColor colorWithRed:230.0/255 green:68.0/255 blue:97.0/255 alpha:1.0]
 
-
 #endif
