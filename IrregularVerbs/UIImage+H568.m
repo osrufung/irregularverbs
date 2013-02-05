236f618d7d97f1b5d238cc1a9e2bd57add384933
Rafa Barberá <rbarberac@gmail.com>
2013-02-05 11:23:44 +0100

    Minor bug in UIImage+H568... if you compute a new image name, better use it ;-)

--- IrregularVerbs/UIImage+H568.m
+++ IrregularVerbs/UIImage+H568.m
@@ -11,24 +11,24 @@
 @implementation UIImage (H568)
 
 + (UIImage *)imageForDeviceHeightWithName:(NSString *)imageName {
-    NSMutableString *imageNameMutable = [imageName mutableCopy];
+    NSMutableString *imageNameWithHeight = [imageName mutableCopy];
     
     if ([[UIScreen mainScreen] bounds].size.height == 568) {        
         //Delete png extension
         NSRange extension = [imageName rangeOfString:@".png" options:NSBackwardsSearch | NSAnchoredSearch];
         if (extension.location != NSNotFound) {
-            [imageNameMutable deleteCharactersInRange:extension];
+            [imageNameWithHeight deleteCharactersInRange:extension];
         }
         //Look for @2x to introduce -568h string
         NSRange retinaAtSymbol = [imageName rangeOfString:@"@2x"];
         if (retinaAtSymbol.location != NSNotFound) {
-            [imageNameMutable insertString:@"-568h" atIndex:retinaAtSymbol.location];
+            [imageNameWithHeight insertString:@"-568h" atIndex:retinaAtSymbol.location];
         } else {
-            [imageNameMutable appendString:@"-568h@2x"];
+            [imageNameWithHeight appendString:@"-568h@2x"];
         }
     }
     
-    return [UIImage imageNamed:imageName];
+    return [UIImage imageNamed:imageNameWithHeight];
 }
 
 @end
